f239b665f9094f04e223a1432603b07360719042
Yashraj8959 <ykushwah7828@gmail.com>
2025-01-27 19:27:47 +0530

    scope in JS

---
+++ JavaScript.js
@@ -0,0 +1,14 @@
+/*
+scope------------------------------>
+
+scope is a certain region of a program where a defined variable can be exist and recognized, beyond that it can't be recognized.
+types of scopes---->
+global scope ---> var a;
+block scope ---- {}
+functional scope-----> function(){}
+
+
+
+
+
+*/
